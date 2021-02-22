f26ffd90c4fffea562cd97de93bd76643fd5a976
LukasJohns <lukas.johns@ocsbstudent.ca>
2021-02-22 16:03:42 +0000

    js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function myInput () {
   age = document.getElementById('check').value
   age = parseInt(age)
   day = document.getElementById('jon').value
-  if ((age > 5 && age < 12)||(day == 'tuesday')) {
+  if ((age > 5 && age < 12)  || (day === 'tuesday')) {
     document.getElementById('response').innerHTML = 'you do not have to pay for admissions'
   } else {
     document.getElementById('response').innerHTML = 'you must pay for admissions'
